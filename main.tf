631101a9a2beaa95830bedfcdb7823c660b4e842
mfaizalbe <mdfaizalehsan@gmail.com>
2026-05-05 21:01:37 +0800

    Add Terraform configuration and GitHub Actions workflow YAML

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "fsctp-ce12-tfstate-bucket"
+    key    = "terraform/state.tfstate"
+    region = "ap-southeast-1"
+  }
+}
